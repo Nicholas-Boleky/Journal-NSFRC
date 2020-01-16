f78d2dcae766aad2c0ff84f3d74bdf6a332c576f
Nicholas Boleky <hotfeet100@hotmail.com>
2020-01-16 13:47:53 -0700

    updated journal

--- Journal+NSFRC copy/Journal+NSFRC/Model Controller/EntryController.swift
+++ Journal+NSFRC copy/Journal+NSFRC/Model Controller/EntryController.swift
@@ -12,11 +12,33 @@ import CoreData
 class EntryController {
     
     static let sharedInstance = EntryController()
-    
-    var entries: [Entry] {
+    // Create a var to access our fetched results controller
+    var fetchedResultsController: NSFetchedResultsController<Entry>
+    // create an init that gives our fetched results controller a value
+    init() {
+        // step 2: create fetchRequest in order to fulfill the parameter requirement of the resultsController initializer
         let fetchRequest: NSFetchRequest<Entry> = Entry.fetchRequest()
-        return (try? CoreDataStack.context.fetch(fetchRequest)) ?? []
+        //step3 accessed the sortdescriptors property on our fetch reuest and told it we wanted our results sorted by timestamp in descending order
+       
+        fetchRequest.sortDescriptors = [NSSortDescriptor(key: "timestamp", ascending: false)]
+        //step 4: filled in parameters our our results controller initializer with our fetch request, our context from our coreDataStack, and passed in nil for section and cache as they are not needed for this app.
+        //step 1: created a let called resultsController that was  NSFetchedResultsController initialized from the vavailable initializer
+        let resultsController: NSFetchedResultsController<Entry> = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: CoreDataStack.context, sectionNameKeyPath: nil, cacheName:
+            nil)
+    //step 5 we assigned our results controller constant as the value of our fetchedResultsController variable (this got rid of the need to return in our initializer
+        fetchedResultsController = resultsController
+      //step 6 accessed our fetched results controller and try to run the performFetch() func given to us by apple
+        //because this func throws we need to wrap in do/try/catch to catch the error if there is one
+        do {
+        try fetchedResultsController.performFetch()
+        } catch {
+            print("there was an error performing fetch: \(error.localizedDescription)")
+        }
     }
+//    var entries: [Entry] {
+//        let fetchRequest: NSFetchRequest<Entry> = Entry.fetchRequest()
+//        return (try? CoreDataStack.context.fetch(fetchRequest)) ?? []
+//    }
     
     //CRUD
     func createEntry(withTitle: String, withBody: String) {
